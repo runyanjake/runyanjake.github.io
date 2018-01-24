7b10fe4d5744768d566b2caaf5b5ab0bf908e505
Jacob Runyan <jmrunyan@ucsc.edu>
2018-01-24 13:46:21 -0800

    Cleaned up source, added init function for js.

---
+++ index.js
@@ -0,0 +1,15 @@
+/****************************************************
+ * index.js
+ * @author Jake Runyan
+ * @desc The Javascript portion of / for the website.
+ *///*************************************************
+  
+/*
+ * This function sets up the webpage upon entry.
+ * It will be run first every time the page loads.
+ */
+function initializePage(){
+    console.log("Initializing page... ");
+    //do stuff.
+    console.log("Done.\n");
+}
